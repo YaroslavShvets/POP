a5fcc46e0904435d922c143e804008ce7dc16ab9
YaroslavShvets <yaroslav.shvets.3707@gmail.com>
2022-05-11 19:39:43 +0300

    1

---
+++ Lab 2/Lab2_C#/Lab2/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Threading;
+
+namespace Program
+{
+    class Program
+    {
+        private static Random rnd = new Random();
+        static void Main(string[] args)
+        {
+            new Thread(() => MainThread()).Start();
+
+        }
+        static void MainThread()
+        {
+            int arrayLen = rnd.Next(1, 10) * 100;
+            int[] array = new int[arrayLen];
+            Thread[] threads = new Thread[arrayLen / 100];
+            MinValue minValue = new MinValue();
+            for (int i = 0; i < arrayLen; i++)
+            {
+                array[i] = rnd.Next(-100000, 100000);
+            }
+            for (int i = 0; i < arrayLen / 100; i++)
+            {
+                int[] subarray = new int[100];
+                Array.Copy(array, i * 100, subarray, 0, 100);
+                threads[i] = new Thread(() => FindMin(subarray, minValue));
+                threads[i].Start();
+            }
+            // Очікування завершення всіх потоків
+            bool notFinished = true;
+            while (notFinished)
+            {
+                foreach (Thread t in threads)
+                {
+                    if (t.ThreadState.ToString() != "Stopped")
+                    {
+                        notFinished = true;
+                        break;
+                    }
+                    notFinished = false;
+                }
+            }
+            Console.WriteLine("Результат: " + minValue.min);
+        }
+
+        static void FindMin(int[] array, MinValue minValue)
+        {
+            int min = Int32.MaxValue;
+            for (int i = 0; i < array.Length; i++)
+            {
+                
+                if (array[i] < min)
+                {
+                    min = array[i];
+                }
+            }
+            minValue.set(min);
+        }
+    }
+    class MinValue
+    {
+        public object locker = new();
+        public int min = Int32.MaxValue;
+
+        public void set(int value)
+        {
+            lock (locker)
+            {
+                min = value < min ? value : min;
+            }
+        }
+
+    }
+}
